e8270c17fde0b573a3c94ae8ff840df44dfecc16
Michael Prieß <email@mpriess.com>
2013-07-06 01:28:10 +0200

    Extended test description

--- src/test/scala/io/pubsub/PubSubServiceSpec.scala
+++ src/test/scala/io/pubsub/PubSubServiceSpec.scala
@@ -7,22 +7,21 @@ import spray.http.MediaTypes._
 import spray.http.HttpBody
 import spray.http.HttpEntity
 import spray.http.ContentType
+import spray.http.StatusCodes
 
 
 object PubSubServiceSpec extends Specification with Specs2RouteTest with PubSubService {
   def actorRefFactory = system
 
-    "A GET request to the subscribe path" in {
-      Get("/subscribe") ~> pubsub ~> check { entityAs[String] must contain("You wanna subscribe?") }
-    }
-
-    "A GET request to the publish path" in {
-      Get("/publish") ~> pubsub ~> check { entityAs[String] must contain("You wanna publish?") }
-    }
-
     "A POST request to the subscribe path" in {
-      val body = HttpEntity.apply(`application/x-www-form-urlencoded`, "hub.callback=foo&hub.mode=bar&hub.topic=foo&hub.verify=foo&hub.lease_seconds=fsds&hub.secret=foo&hub.verify_token=bar")
+      val body = HttpEntity.apply(`application/x-www-form-urlencoded`, "hub.callback=foo&hub.mode=subscribe&hub.topic=foo&hub.verify=sync")
       
-      Post("/subscribe").withEntity(body) ~> pubsub ~> check { entityAs[String] must contain("subscribe") }
+      Post("/subscribe").withEntity(body) ~> pubsub ~> check { status mustEqual StatusCodes.NoContent }
+    }
+    
+    "A POST request to the subscribe path which must be verified" in {
+      val body = HttpEntity.apply(`application/x-www-form-urlencoded`, "hub.callback=foo&hub.mode=subscribe&hub.topic=foo&hub.verify=async")
+      
+      Post("/subscribe").withEntity(body) ~> pubsub ~> check { status mustEqual StatusCodes.Accepted }
     }
 }
